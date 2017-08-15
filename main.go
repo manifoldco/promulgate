8297c75df898883d518f529cb6467d31417d6370
James Bowes <jbowes@repl.ca>
2017-08-15 14:35:46 -0300

    Add a makefile for linting and building

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 	"github.com/urfave/cli"
 )
 
+const version = "dev"
+
 func main() {
 	app := cli.NewApp()
+	app.Version = version
 	app.Usage = "Make releases widely known"
 	app.Commands = cmd.Cmds
 	app.Run(os.Args)
